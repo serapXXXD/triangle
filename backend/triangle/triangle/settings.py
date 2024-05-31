11c09e3a98542d90e2e8ab7927d19ec841b07c5d
serapXXXD <serapXXXD@gmail.com>
2024-05-31 20:03:00 +0300

    add celery, rabbitMQ, redis

--- backend/triangle/triangle/settings.py
+++ backend/triangle/triangle/settings.py
@@ -141,3 +141,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'amqp://myuser:mypassword@localhost:5672/myvhost'
